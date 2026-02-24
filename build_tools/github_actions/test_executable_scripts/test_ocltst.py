e6df5233a40a0d809cc81ed26ad494fee2947a4e
agunashe <ajay.gunashekar@amd.com>
2026-02-24 13:42:54 -0600

    Enable ocltst execution on Rock

---
+++ build_tools/github_actions/test_executable_scripts/test_ocltst.py
@@ -0,0 +1,68 @@
+import logging
+import os
+import shlex
+import subprocess
+from pathlib import Path
+#import glob
+#import shutil
+#import json
+import sys
+import platform
+
+logging.basicConfig(level=logging.INFO)
+THEROCK_BIN_DIR_STR = os.getenv("THEROCK_BIN_DIR")
+if THEROCK_BIN_DIR_STR is None:
+    logging.info(
+        "++ Error: env(THEROCK_BIN_DIR) is not set. Please set it before executing tests."
+    )
+    sys.exit(1)
+THEROCK_BIN_DIR = Path(THEROCK_BIN_DIR_STR)
+SCRIPT_DIR = Path(__file__).resolve().parent
+THEROCK_DIR = SCRIPT_DIR.parent.parent.parent
+os_type = platform.system().lower()
+OCLTST_PATH = str(Path(THEROCK_BIN_DIR).parent / "share" / "opencl" / "ocltst")
+if not os.path.isdir(OCLTST_PATH):
+    logging.info(f"++ Error: ocltst not found in {OCLTST_PATH}")
+    sys.exit(1)
+env = os.environ.copy()
+
+
+def setup_env(env):
+    # catch/ctest framework
+    # Linux
+    #   LD_LIBRARY_PATH needs to be used
+    #   tests are hardcoded to look at THEROCK_BIN_DIR or /opt/rocm/lib path
+    # Windows
+    #   tests load the dlls present in the local exe folder
+    # Set ROCM Path, to find rocm_agent_enum etc
+    ROCM_PATH = Path(THEROCK_BIN_DIR).resolve().parent
+    env["ROCM_PATH"] = str(ROCM_PATH)
+    if platform.system() == "Linux":
+        OCL_LIB_PATH = Path(THEROCK_BIN_DIR).parent / "lib"
+        logging.info(f"++ Setting LD_LIBRARY_PATH={OCL_LIB_PATH}")
+        if "LD_LIBRARY_PATH" in env:
+            env["LD_LIBRARY_PATH"] = f"{OCL_LIB_PATH}:{env['LD_LIBRARY_PATH']}"
+        else:
+            env["LD_LIBRARY_PATH"] = OCL_LIB_PATH
+
+
+def execute_tests(env):
+    if platform.system() == "Linux":
+        cmd = [
+            "./ocltst", "-J", "-m", "oclruntime", "-A", "oclruntime.exclude",
+        ]
+    elif platform.system() == "Windows":
+        cmd = [
+            "ocltst.exe", "-J", "-m", "oclruntime.dll", "-A", "oclruntime.exclude",
+        ]
+    else:
+        logging.info(f"++ Error: unsupported system: {platform.system()}")
+        sys.exit(1)
+
+    logging.info(f"++ Exec [{OCLTST_PATH}]$ {shlex.join(cmd)}")
+    subprocess.run(cmd, cwd=OCLTST_PATH, check=True, env=env)
+
+
+if __name__ == "__main__":
+    setup_env(env)
+    execute_tests(env)
